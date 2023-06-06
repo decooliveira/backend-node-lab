09f104f85fb31c7bf7fc61570cadbdfe3bcd7a94
Deco Oliveira <deco.oliveira@gmail.com>
2023-06-06 16:09:15 -0300

    chore: put and delete

--- index.js
+++ index.js
@@ -26,10 +26,43 @@ router.post("/api/v1/todos", (req, res, next) => {
     if (todo.description === undefined || todo.completed === undefined) {
         return res.status(400).send();
     }
+    
     todos.push(todo);
 
     res.status(201).send();
 });
+
+router.put("/api/v1/todos/:id", (req, res) => {
+
+    const id = req.params.id;
+    const todo = req.body;
+    //get object from array
+    const index = todos.map(t => t.id === Number(id));
+
+    //update the object
+    if (index !== -1) {
+        todos[index].description = todo.description;
+        todos[index].completed = todo.completed;
+    }
+
+    //return response
+    return res.status(200).json(todos);
+});
+
+router.delete("/api/v1/todos/:id", (req, res) => {
+    const id = req.params.id;
+   
+    const index = todos.findIndex(t => t.id === Number(id));
+
+    if (index !== -1) {
+        
+        todos.splice(index, 1);
+    }
+        return res.status(200).json(todos);
+
+})
+
+
 
 app.use(express.json());
 app.use(router);
