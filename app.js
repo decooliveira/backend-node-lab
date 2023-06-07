1e0685caa4e43d81474aff96c5cd03eb0e4aa129
Deco Oliveira <deco.oliveira@gmail.com>
2023-06-07 17:02:32 -0300

    chore: remove node_modules

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const router = require('./router');
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
